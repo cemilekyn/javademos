deeca849a31257c1e3ea3325adc4beccb01e43c8
cemilekyn <114016285+cemilekyn@users.noreply.github.com>
2022-09-21 18:42:49 +0300

    javademos
    java kamp kodları

---
+++ javaHelloWorld/datatypes/datatypes/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double sayi =1.2;
+        sayi= -128;
+
+        char karakter = 'A';
+
+        boolean dogruMu=false;
+        
+        System.out.println("Hello world!");
+    }
+}
